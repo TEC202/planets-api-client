8057f1446222e83a8013d8d72af7b1e242e612e7
Tyler-Chandler <tyler.chandler@protonmail.com>
2023-07-03 09:36:49 -0500

    Init table setup, finished data mapping

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ExpectedTableData, ExpectedAPIResponse, Column } from './types'
 import './App.css';
-import { mapPlanetDataToTableData, sortObjArrayByKey } from './util';
+import { BASE_URL, mapPlanetDataToTableData, sortObjArrayByKey, shouldWrapWithLink } from './util';
 
 function App() {
   const [page, setPage] = React.useState(1)
@@ -21,14 +21,14 @@ function App() {
     setIsLoading(true)
     setErrorMessage('')
     try {
-      const response = await fetch(`https://swapi.dev/api/planets/?page=${pageNumber}`)
+      const response = await fetch(`${BASE_URL}planets/?page=${pageNumber}`)
       if (!response.ok) throw new Error('Error getting data')
       const data = await response.json() as ExpectedAPIResponse
       if (!data.results || !data.results.length) {
         setErrorMessage('No data found')
         return setIsLoading(false)
       }
-      
+
       let mappedData = data.results.map((planet) => {
         return mapPlanetDataToTableData(planet)
       })
@@ -58,8 +58,33 @@ function App() {
         <h1>Planets</h1>
       </header>
       <main>
+        {isLoading 
+          ? <div>Loading...</div> 
+          : <><table>
+              <thead>
+                <tr>
+                  {columns.length ? columns.map((column, index) => {
+                    return <th key={index}>{column.label}</th>
+                  }) : null}
+                </tr>
+              </thead>
+              <tbody>
+                {(tableData && tableData.length) ? tableData.map((row, rowIndex) => {
+                  return <tr key={rowIndex}>
+                    {columns.map((column, colIndex) => {
+                      return <td key={colIndex}>
+                        {shouldWrapWithLink(column.key, row) 
+                        ? <a href={row['url']} target='_blank' rel="noreferrer noopener">{row[column.key]}</a> 
+                        : row[column.key]}
+                      </td>
+                    })}
+                  </tr>
+                }) : null}
+              </tbody>
+            </table></>
+        }
 
-
+        { errorMessage ? <div>{errorMessage}</div>  : null}
       </main>
     </div>
   );
--- src/util.ts
+++ src/util.ts
@@ -1,19 +1,63 @@
 import { APIPlanetData, ExpectedTableData, Direction } from "./types";
 
+const BASE_URL = 'https://swapi.dev/api/'
+const UNKNOWN_VALUE_SUBSTITUTE = '?'
 
 function mapPlanetDataToTableData(planetData: APIPlanetData): ExpectedTableData {
-    // WIP for table setup -- needs formatting, calculations, etc
+    // multiple operations need to happen here in the mapping:
+    //  - Convert all values of 'unknown' to '?' (assumption: except for url)
+    //  - Convert all values representing numbers to a formatted string with spaces for thousands
+    //  - Calculate the surface water area based on the percentage of the planet covered in water and the diameter of the planet
+
+    let numberOfResidents = typeof planetData.residents === 'string' ? convertUnknownValues(planetData.residents) : planetData.residents.length.toString()
+    let surfaceWater = (convertUnknownValues(planetData.surface_water) === UNKNOWN_VALUE_SUBSTITUTE ||  convertUnknownValues(planetData.diameter) === UNKNOWN_VALUE_SUBSTITUTE)
+        ? UNKNOWN_VALUE_SUBSTITUTE 
+        : calculateSphericalSurfaceAreaCovered(planetData.surface_water, planetData.diameter)
     let tableData: ExpectedTableData = {
-        name: planetData.name,
+        name: convertUnknownValues(planetData.name),
         url: planetData.url,
-        climate: planetData.climate,
-        numberOfResidents: planetData.residents.length.toString(),
-        terrain: planetData.terrain,
-        population: planetData.population,
-        surfaceWater: planetData.surface_water // TODO: calculate surface water area
+        climate: convertUnknownValues(planetData.climate),
+        numberOfResidents: convertToFormattedNumber(numberOfResidents),
+        terrain: convertUnknownValues(planetData.terrain),
+        population: convertToFormattedNumber(planetData.population),
+        surfaceWater: convertToFormattedNumber(surfaceWater)
     }
     return tableData
 }
+
+/*
+    * Convert all values of 'unknown' to '?'
+    * Assumption: only strict string values of 'unknown' (case-sensitive) should be converted not ' unknown ' or 'UNKNOWN'
+*/
+function convertUnknownValues(incomingValue: any): any {
+    if (incomingValue === 'unknown') return UNKNOWN_VALUE_SUBSTITUTE
+    else return incomingValue
+}
+
+function convertToFormattedNumber(incomingValue: string | number): string {
+    let formattedNumber = convertUnknownValues(incomingValue)
+    if (formattedNumber === UNKNOWN_VALUE_SUBSTITUTE) return formattedNumber
+
+    formattedNumber = Number(formattedNumber)
+    // toLocaleString() will add commas for thousands so then we will replace commas with spaces for the desired format
+    formattedNumber = formattedNumber.toLocaleString('en-US').split(',').join(' ')
+    return formattedNumber
+
+}
+
+
+/*
+    * Calculate the surface area covered (in a given unit of area e.g. km2) based on the percentage of the surface area covered and the diameter of the planet
+*/
+function calculateSphericalSurfaceAreaCovered(percentCovered: number | string, diameter: number | string): number {
+    percentCovered = Number(percentCovered);
+    diameter = Number(diameter);
+    let radius = diameter / 2;
+    var surfaceAreaOfSphere = 4 * Math.PI * Math.pow(radius, 2); // find the surface area of the sphere
+    var surfaceAreaCovered = (percentCovered / 100) * surfaceAreaOfSphere; // find the area covered (in whichever unit was used for diameter)
+    return Math.round(surfaceAreaCovered); // round to nearest whole number
+  }
+  
 
 function sortObjArrayByKey(objArray: any[], key: string, direction: Direction) {
     return objArray.sort((a, b) => {
@@ -25,7 +69,15 @@ function sortObjArrayByKey(objArray: any[], key: string, direction: Direction) {
     })
 }
 
+function shouldWrapWithLink(columnKey: string, row: any): boolean {
+    if (columnKey !== 'name') return false
+    if (!row || !row['url']) return false
+    return row['url'].startsWith(BASE_URL)
+  }
+
 export {
+    BASE_URL,
     mapPlanetDataToTableData,
     sortObjArrayByKey,
+    shouldWrapWithLink,
 }
